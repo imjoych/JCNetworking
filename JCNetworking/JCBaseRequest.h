8b89bbeeb79bc178f07f62fa07fbb473cb943bbb
陈建军 <jianjun16@staff.weibo.com>
2018-02-27 10:41:17 +0800

    Update comments

--- JCNetworking/JCBaseRequest.h
+++ JCNetworking/JCBaseRequest.h
@@ -16,8 +16,8 @@
 
 /**
  * Json data translate into JCModel object.
- * @params json Serialization data which can be parsed.
- * @params error Errors occured when the data is not a normal json.
+ * @param json Serialization data which can be parsed.
+ * @param error Errors occured when the data is not a normal json.
  * @return Json data will be parsed when it's NSDictionary / NSData / NSString and returns object of JCModel or it's subclass, other returns nil.
  */
 + (instancetype)objWithJson:(id)json error:(NSError **)error;
